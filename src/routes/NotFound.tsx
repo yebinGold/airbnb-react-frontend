8dccb0b83e1f98dd1cb81cec984724f75caa9f78
Yebin Kim <rladpqls07@gmail.com>
2022-11-25 02:33:20 +0900

    feat: 404 페이지 내용 생성

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { Heading, VStack, Text, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-    return (
-        <div>
-            Not found
-        </div>
-    );
+  return (
+    <VStack bg="gray.100" minHeight={"100vh"} justifyContent={"center"}>
+      <Heading marginBottom={"2rem"}>Page Not Found</Heading>
+      <Text>It seems you're lost..!</Text>
+      <Link to="/">
+        <Button colorScheme={"facebook"} variant={"link"}>
+          Go Home &rarr;
+        </Button>
+      </Link>
+    </VStack>
+  );
 };
 
 export default NotFound;

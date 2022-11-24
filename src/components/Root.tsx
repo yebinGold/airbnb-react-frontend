f3e7bc124deb9031c673e0d18876b64d5c47fb52
Yebin Kim <rladpqls07@gmail.com>
2022-11-25 02:57:44 +0900

    feat: build Header in Root component

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,11 +1,26 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
+import { FaAirbnb } from "react-icons/fa";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
-      i'm root
+    <Box>
+      <HStack
+        px={"5"}
+        py={"7"}
+        borderBottomWidth={1}
+        justifyContent={"space-between"}
+      >
+        <Box color={"red.500"}>
+          <FaAirbnb size={"38"} />
+        </Box>
+        <HStack spacing={"2"}>
+          <Button size={"sm"}>Log In</Button>
+          <Button colorScheme={"red"} size={"sm"}>Sign Up</Button>
+        </HStack>
+      </HStack>
       <Outlet /> {/** 자식 컴포넌트 렌더링 될 위치 */}
-    </div>
+    </Box>
   );
 };
 

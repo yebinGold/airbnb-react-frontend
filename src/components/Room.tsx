85960238bb6d98360cf0263492e0707df5c2e0d3
Yebin Kim <rladpqls07@gmail.com>
2022-11-25 18:07:15 +0900

    feat: build Home component

---
+++ src/components/Room.tsx
@@ -0,0 +1,53 @@
+import {
+  Grid,
+  Box,
+  Image,
+  VStack,
+  Button,
+  Text,
+  HStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { FaRegHeart, FaStar } from "react-icons/fa";
+
+const Room = () => {
+  const grayTextColor = useColorModeValue("gray.600", "gray.300");
+  return (
+    <VStack alignItems={"flex-start"} spacing={1}>
+      <Box position="relative" rounded="3xl" mb={2} overflow="hidden">
+        <Image
+          minH="280"
+          src="https://a0.muscache.com/im/pictures/miso/Hosting-717134404264905813/original/dfe9fd1e-a010-43c9-b546-0bbc7d59f7f3.jpeg?im_w=720"
+        />
+        <Button
+          variant={"unstyled"}
+          position="absolute"
+          top={2}
+          right={0}
+          color={"white"}
+        >
+          <FaRegHeart size={20} />
+        </Button>
+      </Box>
+      <Box>
+        <Grid gap={2} templateColumns={"4fr 1fr"}>
+          <Text noOfLines={1} fontSize={"md"} as="b">
+            케이프타운, 웨스턴케이프 주, 남아프리카
+          </Text>
+          <HStack spacing={1}>
+            <FaStar size={15} />
+            <Text>4.0</Text>
+          </HStack>
+        </Grid>
+        <Text fontSize={"sm"} color={grayTextColor}>
+          Seoul, South Korea
+        </Text>
+      </Box>
+      <Text fontSize={"sm"} color={grayTextColor}>
+        <Text as="b">$150</Text> / night
+      </Text>
+    </VStack>
+  );
+};
+
+export default Room;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { Grid } from "@chakra-ui/react";
+import Room from "../components/Room";
 
 const Home = () => {
-    return (
-        <div>
-            home
-        </div>
-    );
+  return (
+    <Grid
+      mt={10}
+      px={{
+        base: 10,
+        lg: 40,
+      }}
+      columnGap={4}
+      rowGap={10}
+      templateColumns={{
+        sm: "1fr",
+        md: "repeat(2, 1fr)",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
+    >
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 2, 4, 8, 9, 2, 1].map((idx) => (
+        <Room key={idx} />
+      ))}
+    </Grid>
+  );
 };
 
 export default Home;
